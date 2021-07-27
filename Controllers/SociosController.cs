6b291512d06b9261bceed514bee7a95d8fe6df63
Christian Mora <cmoralopez@gmail.com>
2021-07-26 21:25:04 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/SociosController.cs
@@ -0,0 +1,49 @@
+﻿using Examen.Data;
+using Examen.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen.Controllers
+{
+    public class SociosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SociosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            List<Socio> socios = new List<Socio>();
+            socios = _context.Socios.ToList();
+            return View(socios);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Socio socio)
+        {
+            try
+            {
+                _context.Add(socio);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Cuenta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen.Models
+{
+    public class Cuenta
+    {
+        public object Numero { get; internal set; }
+        public object CodigoSocio { get; internal set; }
+        public object SaldoTotal { get; internal set; }
+        public object CodigoSocioNavigation { get; internal set; }
+        public object Estado { get; internal set; }
+
+        public partial class Cuentas
+        {
+            public string Numero { get; set; }
+            public string SaldoTotal { get; set; }
+            public string CodigoSocio { get; set; }
+            public int Estado { get; set; }
+
+            public virtual Socio CodigoSocioNavigation { get; set; }
+        }
+    }
+}
